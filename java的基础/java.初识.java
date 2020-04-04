366459680e09594c999488417e5219f377accad9
Waynexia888 <qiwuwaynexia@gmail.com>
2020-04-04 01:19:03 -0700

    updated

---
+++ java的基础/java.初识.java
@@ -0,0 +1,10 @@
+// JDK, JRE 和 JVM 三者的关系:
+// - JVM： java虚拟机
+// - JRE： java运行环境，它是包含JVM的
+// - JDK： java开发工具包，它是把JRE和JVM都包含起来了的
+// JRE  = JVM + JavaSE标准类库
+// JDK = JRE + 开发工具集（例如Javac编译工具等）
+
+// Java程序执行过程：
+// 源文件（Projram.java) -> 编译器（compiler） -> 字节码文件（Program.class) 
+// -> 解释器（interpreter） -> 电脑Program
