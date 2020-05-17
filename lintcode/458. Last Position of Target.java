dcec4392508bbaf434323813ab351d734ce94995
Waynexia888 <qiwuwaynexia@gmail.com>
2020-05-16 23:05:48 -0700

    updated

---
+++ lintcode/458. Last Position of Target.java
@@ -0,0 +1,34 @@
+public class 458. Last Position of Target Solution {
+    /**
+     * @param nums: An integer array sorted in ascending order
+     * @param target: An integer
+     * @return: An integer
+     */
+    public int lastPosition(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                start = mid;
+            } else if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        if (nums[end] == target) {
+            return end;
+        }
+        
+        if (nums[start] == target) {
+            return start;
+        }
+        
+        return -1;
+    }
+}
