c1b3aa89c3837e91790d586c0425f452845b3de6
Waynexia888 <qiwuwaynexia@gmail.com>
2020-05-20 16:34:00 -0700

    updated

---
+++ lintcode/9. Fizz Buzz.java
@@ -0,0 +1,23 @@
+public class Fizz_Buzz {
+    /**
+     * @param n: An integer
+     * @return: A list of strings.
+     */
+    public ArrayList<String> fizzBuzz(int n) {
+        // write your code here
+        ArrayList<String> results = new ArrayList<String>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                results.add("fizz buzz");
+            } else if (i % 3 == 0) {
+                results.add("fizz");
+            } else if (i % 5 == 0) {
+                results.add("buzz");
+            } else {
+                results.add(String.valueOf(i)); // i + ""
+            }
+        }
+
+        return results;
+    }
+}
