6ff7e8bee5c40cf494f1a00c6bd50efbef483c80
Waynexia888 <qiwuwaynexia@gmail.com>
2020-05-20 19:58:30 -0700

    updated

---
+++ lintcode/283. Max of 3 Numbers.java
@@ -0,0 +1,19 @@
+public class Max_of_3_Numbers {
+    /**
+     * @param num1: An integer
+     * @param num2: An integer
+     * @param num3: An integer
+     * @return: an interger
+     */
+    public int maxOfThreeNumbers(int num1, int num2, int num3) {
+        // write your code here
+        int maxNumber = num1;
+        if (num1 > num2 && num1 > num3) {
+            return num1;
+        } else if (num2 > num1 && num2 > num3) {
+            return num2;
+        } else {
+            return num3;
+        }
+    }
+}
