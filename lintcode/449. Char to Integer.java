93bfb23acefc296642a067f03edd4a847f5d7579
Waynexia888 <qiwuwaynexia@gmail.com>
2020-05-20 19:00:49 -0700

    updated

---
+++ lintcode/449. Char to Integer.java
@@ -0,0 +1,11 @@
+public class Char_to_Integer {
+    /**
+     * @param character: a character
+     * @return: An integer
+     */
+    public int charToInteger(char character) {
+        // int类型和char类型之间的转换
+        int number = (int) character;
+        return number;
+    }
+}
