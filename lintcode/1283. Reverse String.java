69b454fbe44abaf5ea641fd1a9cd6c4e034d2056
Waynexia888 <qiwuwaynexia@gmail.com>
2020-05-23 01:22:02 -0700

    updated

--- lintcode/1283. Reverse String.java
+++ lintcode/1283. Reverse String.java
@@ -8,19 +8,34 @@ public class Reverse_String {
         // java 不允许字符串的修改
         // 所以先创建一个数组,数组里都是字符
 
-        if (s == null || s.length() <= 1) {
+        // if (s == null || s.length() <= 1) {
+        // return s;
+        // }
+
+        // char[] chars = new char[s.length()];
+        // int left = 0, right = s.length() - 1;
+        // while (left <= right) {
+        // chars[left] = s.charAt(right);
+        // chars[right] = s.charAt(left);
+        // left++;
+        // right--;
+        // }
+        // return new String(chars);
+
+        // 可以把string转变成数组， toCharArray()
+        if (s == null || s.length() < 2) {
             return s;
         }
 
-        char[] chars = new char[s.length()];
+        char[] chars = s.toCharArray();
         int left = 0, right = s.length() - 1;
-        while (left <= right) {
-            chars[left] = s.charAt(right);
-            chars[right] = s.charAt(left);
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
             left++;
             right--;
         }
         return new String(chars);
-
     }
 }
