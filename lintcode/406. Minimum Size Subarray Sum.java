fdd582e06522b099755adc2d73e3e22a8b622bbe
Waynexia888 <qiwuwaynexia@gmail.com>
2020-05-23 21:50:13 -0700

    updated

---
+++ lintcode/406. Minimum Size Subarray Sum.java
@@ -0,0 +1,39 @@
+public class Minimum_Size_Subarray_Sum {
+    /**
+     * @param nums: an array of integers
+     * @param s:    An integer
+     * @return: an integer representing the minimum size of subarray
+     */
+    public int minimumSize(int[] nums, int s) {
+        // 同向双指针问题：
+        // 首先设置一个滑动窗口
+
+        if (nums == null || nums.length < 1) {
+            return -1;
+        }
+
+        int left = 0, right = -1;
+        int sum = 0;
+        int minLength = nums.length + 1;
+
+        while (left < nums.length) {
+            if (right + 1 < nums.length && sum < s) {
+                right++; // 要保证right不超出范围
+                sum += nums[right];
+            } else { // sum >= s
+                sum -= nums[left];
+                left++;
+            }
+
+            if (sum >= s) {
+                minLength = Math.min(minLength, right - left + 1);
+            }
+        }
+
+        if (minLength == nums.length + 1) {
+            return -1;
+        }
+        return minLength;
+
+    }
+}
