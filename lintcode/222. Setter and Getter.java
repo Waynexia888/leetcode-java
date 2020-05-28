2bd7319b29c962580f44437c52797b44851ff58c
Waynexia888 <qiwuwaynexia@gmail.com>
2020-05-28 14:24:53 -0700

    updated

---
+++ lintcode/222. Setter and Getter.java
@@ -0,0 +1,21 @@
+public class School {
+    /*
+     * Declare a private attribute *name* of type string.
+     */
+    private String name;
+
+    /**
+     * Declare a setter method `setName` which expect a parameter *name*.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Declare a getter method `getName` which expect no parameter and return the
+     * name of this school
+     */
+    public String getName() {
+        return this.name;
+    }
+}
