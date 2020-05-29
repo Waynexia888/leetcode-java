f37b391a29a9537929e523ed0bba16705a954904
Waynexia888 <qiwuwaynexia@gmail.com>
2020-05-28 17:30:14 -0700

    updated

--- lintcode/463. Sort Integers.java
+++ lintcode/463. Sort Integers.java
@@ -116,3 +116,28 @@ public class Solution {
         quickSort(A, left, end);
     }
 }
+
+/////////////////////////////////////////////////////////
+// 选择排序， Selection Sort
+
+public class Solution {
+    /**
+     * @param A: an integer array
+     * @return: nothing
+     */
+    public void sortIntegers(int[] A) {
+        // selection sort
+        // 时间复杂度O(n^2), 空间复杂度O(1)
+        for (int i = 0; i < A.length - 1; i++) {
+            int minNumber = i;
+            for (int j = i + 1; j < A.length; j++) {
+                if (A[j] < A[minNumber]) {
+                    minNumber = j;
+                }
+            }
+            int temp = A[i];
+            A[i] = A[minNumber];
+            A[minNumber] = temp;
+        }
+    }
+}
