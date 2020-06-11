882c11fdf1b47b1c6706182d846aad8540de82d4
Waynexia888 <qiwuwaynexia@gmail.com>
2020-06-11 14:39:33 -0700

    updated

---
+++ lintcode/35. Reverse Linked List.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param head: n
+     * @return: The new head of reversed linked list.
+     */
+    public ListNode reverse(ListNode head) {
+        // 参考视频： https://www.youtube.com/watch?v=XDO6I8jxHtA
+        ListNode prev = null;
+        while (head != null) {
+            ListNode temp = head; // 用临时变量temp记录当前的节点
+            head = head.next;     // 然后不断往后遍历
+            temp.next = prev;     // 把当前节点指向prev这个节点
+            prev = temp;          // 每次更新prev，即往后移一步
+        }
+        return prev;
+    }
+}
