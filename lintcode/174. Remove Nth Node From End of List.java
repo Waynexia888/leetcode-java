5aa670ac9ea2addac8429d08fa000e2de4d53ec9
Waynexia888 <qiwuwaynexia@gmail.com>
2020-06-11 12:46:09 -0700

    updated

---
+++ lintcode/174. Remove Nth Node From End of List.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for ListNode public class ListNode { int val; ListNode next;
+ * ListNode(int x) { val = x; next = null; } }
+ */
+
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @param n:    An integer
+     * @return: The head of linked list.
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // 快慢指针
+        // 先让快指针走n步，然后快慢一起走，当快指针走到尾的时候， 慢指针刚好走到要删除节点的前一个位置，然后完成删除操作；
+        if (n <= 0)
+            return null;
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode slow = dummy, fast = dummy;
+        for (int i = 0; i <= n; i++) {
+            if (fast == null)
+                return head;
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
