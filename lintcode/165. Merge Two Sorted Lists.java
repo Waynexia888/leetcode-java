782d9c9317b90f92f2aaad923dad4794f2b5146f
Waynexia888 <qiwuwaynexia@gmail.com>
2020-06-11 13:25:01 -0700

    updated

---
+++ lintcode/165. Merge Two Sorted Lists.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for ListNode public class ListNode { int val; ListNode next;
+ * ListNode(int x) { val = x; next = null; } }
+ */
+
+public class Solution {
+    /**
+     * @param l1: ListNode l1 is the head of the linked list
+     * @param l2: ListNode l2 is the head of the linked list
+     * @return: ListNode head of linked list
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // write your code here
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+
+        if (l1 != null) {
+            tail.next = l1;
+        } else {
+            tail.next = l2;
+        }
+
+        return dummy.next;
+    }
+}
