dbc2e50e83d668e7d4154ccd2e893153ddd9fd44
Waynexia888 <qiwuwaynexia@gmail.com>
2020-06-10 21:46:47 -0700

    updated

---
+++ lintcode/452. Remove Linked List Elements.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param head: a ListNode
+     * @param val: An integer
+     * @return: a ListNode
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        // write your code here
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        head = dummy;
+        
+        while (head.next != null) {
+            if (head.next.val == val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+        return dummy.next;
+    }
+}
