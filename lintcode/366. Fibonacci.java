3d7aea752a989d911decd1e34fa1bc6ce046463b
Waynexia888 <qiwuwaynexia@gmail.com>
2020-06-13 00:26:04 -0700

    updated

---
+++ lintcode/366. Fibonacci.java
@@ -0,0 +1,20 @@
+public class Solution {
+    /**
+     * @param n: an integer
+     * @return: an ineger f(n)
+     */
+    public int fibonacci(int n) {
+        // 纯递归，时间复杂度O(n), 空间复杂度O(n),会stack overflow
+        // if (n == 1) return 0;
+        // if (n == 2) return 1;
+
+        // return fibonacci(n - 1) + fibonacci(n - 2);
+        int a = 0, b = 1;
+        for (int i = 0; i < n - 1; i++) { // n = 5, return 3, loop 4次
+            int c = a + b; // 1, 2, 3, 5
+            a = b; // 1, 1, 2, 3
+            b = c; // 1, 2, 3, 5
+        }
+        return a;
+    }
+}
