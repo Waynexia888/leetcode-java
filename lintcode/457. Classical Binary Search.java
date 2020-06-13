abc34b8d035efcb31c8e824a4b4e03a9e91c642c
Waynexia888 <qiwuwaynexia@gmail.com>
2020-06-13 01:10:59 -0700

    updated

--- lintcode/457. Classical Binary Search.java
+++ lintcode/457. Classical Binary Search.java
@@ -32,3 +32,34 @@ public class Classical_Binary_Search_Solution {
         return -1;
     }
 }
+
+/////////////////////////////////////////////////////////////////////////
+public class Solution {
+    /**
+     * @param nums:   An integer array sorted in ascending order
+     * @param target: An integer
+     * @return: An integer
+     */
+    public int findPosition(int[] nums, int target) {
+        // 用递归的方法实现二分法
+        return binarySearch(nums, 0, nums.length - 1, target);
+    }
+
+    public int binarySearch(int[] nums, int start, int end, int target) {
+        if (start > end) {
+            return -1;
+        }
+
+        int mid = (start + end) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        if (nums[mid] < target) {
+            return binarySearch(nums, mid + 1, end, target);
+        }
+        return binarySearch(nums, start, mid - 1, target);
+
+    }
+}
