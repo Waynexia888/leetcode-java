eec0958d4c0d24c3c47230f10815c20048db9a60
Waynexia888 <qiwuwaynexia@gmail.com>
2020-06-14 23:30:41 -0700

    updated

---
+++ lintcode/480. Binary Tree Paths.java
@@ -0,0 +1,39 @@
+/**
+ * Definition of TreeNode: public class TreeNode { public int val; public
+ * TreeNode left, right; public TreeNode(int val) { this.val = val; this.left =
+ * this.right = null; } }
+ */
+
+public class Solution {
+    /**
+     * @param root: the root of the binary tree
+     * @return: all root-to-leaf paths
+     */
+    public List<String> binaryTreePaths(TreeNode root) {
+        // divide conquer
+        List<String> paths = new ArrayList<>();
+
+        if (root == null) {
+            return paths;
+        }
+
+        List<String> leftPaths = binaryTreePaths(root.left);
+        List<String> rightPaths = binaryTreePaths(root.right);
+
+        for (String path : leftPaths) {
+            paths.add(root.val + "->" + path);
+        }
+
+        for (String path : rightPaths) {
+            paths.add(root.val + "->" + path);
+        }
+
+        // root is a leaf
+        if (paths.size() == 0) {
+            paths.add("" + root.val);
+        }
+
+        return paths;
+
+    }
+}
