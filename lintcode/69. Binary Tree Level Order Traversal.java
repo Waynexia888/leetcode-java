33c3a8819fb899eaa235e41d27dba1065ea209cf
Waynexia888 <qiwuwaynexia@gmail.com>
2020-06-17 20:27:12 -0700

    updated

---
+++ lintcode/69. Binary Tree Level Order Traversal.java
@@ -0,0 +1,42 @@
+/**
+ * Definition of TreeNode: public class TreeNode { public int val; public
+ * TreeNode left, right; public TreeNode(int val) { this.val = val; this.left =
+ * this.right = null; } }
+ */
+
+public class Solution {
+    /**
+     * @param root: A Tree
+     * @return: Level order a list of lists of integer
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        // 使用BFS实现二叉树的层级遍历
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode cur = queue.poll();
+                level.add(cur.val);
+
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+            result.add(level);
+        }
+        return result;
+    }
+}
