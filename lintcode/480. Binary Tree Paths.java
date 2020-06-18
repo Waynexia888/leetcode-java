164990132b344617baa920f7ff1a4bd46e738cf6
Waynexia888 <qiwuwaynexia@gmail.com>
2020-06-18 15:59:40 -0700

    updated

--- lintcode/480. Binary Tree Paths.java
+++ lintcode/480. Binary Tree Paths.java
@@ -1,3 +1,44 @@
+// /**
+//  * Definition of TreeNode: public class TreeNode { public int val; public
+//  * TreeNode left, right; public TreeNode(int val) { this.val = val; this.left =
+//  * this.right = null; } }
+//  */
+
+// public class Solution {
+//     /**
+//      * @param root: the root of the binary tree
+//      * @return: all root-to-leaf paths
+//      */
+//     public List<String> binaryTreePaths(TreeNode root) {
+//         // divide conquer
+//         List<String> paths = new ArrayList<>();
+
+//         if (root == null) {
+//             return paths;
+//         }
+
+//         List<String> leftPaths = binaryTreePaths(root.left);
+//         List<String> rightPaths = binaryTreePaths(root.right);
+
+//         for (String path : leftPaths) {
+//             paths.add(root.val + "->" + path);
+//         }
+
+//         for (String path : rightPaths) {
+//             paths.add(root.val + "->" + path);
+//         }
+
+//         // root is a leaf
+//         if (paths.size() == 0) {
+//             paths.add("" + root.val);
+//         }
+
+//         return paths;
+
+//     }
+// }
+
+////////////////////////////////////////////////////////////
 /**
  * Definition of TreeNode: public class TreeNode { public int val; public
  * TreeNode left, right; public TreeNode(int val) { this.val = val; this.left =
@@ -10,30 +51,31 @@ public class Solution {
      * @return: all root-to-leaf paths
      */
     public List<String> binaryTreePaths(TreeNode root) {
-        // divide conquer
-        List<String> paths = new ArrayList<>();
+        // write your code here
+        List<String> result = new ArrayList<>();
 
         if (root == null) {
-            return paths;
+            return result;
+        }
+
+        // 99%的题，不用单独处理叶子节点
+        // 这里需要单独处理的原因是： root是None的结果，没有办法用于构造root是叶子的结果
+        if (root.left == null && root.right == null) {
+            result.add(root.val + "");
         }
 
         List<String> leftPaths = binaryTreePaths(root.left);
         List<String> rightPaths = binaryTreePaths(root.right);
 
         for (String path : leftPaths) {
-            paths.add(root.val + "->" + path);
+            result.add(root.val + "->" + path);
         }
 
         for (String path : rightPaths) {
-            paths.add(root.val + "->" + path);
+            result.add(root.val + "->" + path);
         }
 
-        // root is a leaf
-        if (paths.size() == 0) {
-            paths.add("" + root.val);
-        }
-
-        return paths;
+        return result;
 
     }
 }
