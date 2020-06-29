e950d731211856425acf4408b9e697c4d1628e35
Waynexia888 <qiwuwaynexia@gmail.com>
2020-06-28 19:22:36 -0700

    updated

---
+++ lintcode/70. Binary Tree Level Order Traversal II.java
@@ -0,0 +1,43 @@
+/**
+ * Definition of TreeNode: public class TreeNode { public int val; public
+ * TreeNode left, right; public TreeNode(int val) { this.val = val; this.left =
+ * this.right = null; } }
+ */
+
+public class Solution {
+    /**
+     * @param root: A tree
+     * @return: buttom-up level order a list of lists of integer
+     */
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        // write your code here
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(level);
+        }
+        Collections.reverse(result);
+        return result;
+    }
+}
