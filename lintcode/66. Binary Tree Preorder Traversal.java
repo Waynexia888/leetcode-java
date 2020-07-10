4fdb2712da138ecc0a895a86a11a421f85f1ecf8
Waynexia888 <qiwuwaynexia@gmail.com>
2020-07-09 18:25:01 -0700

    updated

--- lintcode/66. Binary Tree Preorder Traversal.java
+++ lintcode/66. Binary Tree Preorder Traversal.java
@@ -40,3 +40,47 @@ public class Solution {
 
 ////////////////////////////////////////////////////////////////
 // 非递归实现前序遍历
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: A Tree
+     * @return: Preorder in ArrayList which contains node values.
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        // non-recursion, 先序遍历，根左右
+        // 先放根， 然后放右， 再放左
+        List<Integer> results = new ArrayList<>();
+        if (root == null) {
+            return results;
+        }
+        
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            results.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return results;
+    }
+}
+
