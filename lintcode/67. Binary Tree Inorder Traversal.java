f6f0323b2293aea378025b469e32e688844433f3
Waynexia888 <qiwuwaynexia@gmail.com>
2020-07-09 18:59:02 -0700

    updated

--- lintcode/67. Binary Tree Inorder Traversal.java
+++ lintcode/67. Binary Tree Inorder Traversal.java
@@ -39,3 +39,50 @@ public class Solution {
 
 ////////////////////////////////////////////////////////////////
 // 非递归实现中序遍历
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: A Tree
+     * @return: Inorder in ArrayList which contains node values.
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        // non-recursion 实现中序遍历, 左根右
+        List<Integer> results = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        if (root == null) {
+            return results;
+        }
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+        System.out.println(stack);
+        
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            System.out.println(node);
+            results.add(node.val);
+            
+            if (node.right != null) {
+                TreeNode cur = node.right;
+                while (cur != null) {
+                    stack.push(cur);
+                    cur = cur.left;
+                }
+            }
+        }
+        return results;
+    }
+}
