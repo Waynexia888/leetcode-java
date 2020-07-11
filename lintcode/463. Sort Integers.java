2327d7ee466ffa9f7e47e69719fbb3349dffc3fd
Waynexia888 <qiwuwaynexia@gmail.com>
2020-07-10 19:52:19 -0700

    updated

--- lintcode/463. Sort Integers.java
+++ lintcode/463. Sort Integers.java
@@ -128,6 +128,8 @@ public class Solution {
     public void sortIntegers(int[] A) {
         // selection sort
         // 时间复杂度O(n^2), 空间复杂度O(1)
+        // 它的工作原理是：第一次从待排序的数据元素中选出最小（或最大）的一个元素，
+        // 存放在序列的起始位置，然后再从剩余的未排序元素中寻找到最小（大）元素
         for (int i = 0; i < A.length - 1; i++) {
             int minNumber = i;
             for (int j = i + 1; j < A.length; j++) {
