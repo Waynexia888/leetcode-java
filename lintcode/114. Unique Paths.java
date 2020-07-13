0f864342a2ac5b26d0270f5fca9156ccd29bf2ed
Waynexia888 <qiwuwaynexia@gmail.com>
2020-07-12 17:54:51 -0700

    updated

--- lintcode/114. Unique Paths.java
+++ lintcode/114. Unique Paths.java
@@ -5,21 +5,28 @@ public class Solution {
      * @return: An integer
      */
     public int uniquePaths(int m, int n) {
-        // f[i][j]为机器人有多少种方式从左上角走到(i, j)
-        // 初始化：对于第一行 f[0][j]， 第一列 f[i][0]，前面都只可能有一条路径
-        // 状态方程： f[i][j] = f[i - 1][j] + f[i][j - 1]
-        // 时间复杂度: O(m * n); 空间复杂度: O(m * n)
-        int[][] f = new int[m][n];
-
+        // 坐标型动态规划 - 自顶向下
+        // state 状态; dp[i][j] 表示从0, 0 到i, j 的方案总数
+        int[][] dp = new int[m][n];
+        
+        // 初始化initialize: 第0行第0列的前一步只能有一个方向过来
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 || j == 0) {
-                    f[i][j] = 1;
-                    continue;
-                }
-                f[i][j] = f[i - 1][j] + f[i][j - 1];
+            dp[i][0] = 1;
+        }
+        
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+        
+        
+        // 方程function: dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
-        return f[m - 1][n - 1];
+
+        // 答案answer: dp[m - 1][n - 1]
+        return dp[m - 1][n - 1];
     }
 }
