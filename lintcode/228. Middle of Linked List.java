0b7e3bebc0d23a6d9ac6d3105b1ee74c8e78a9e8
Waynexia888 <qiwuwaynexia@gmail.com>
2020-07-15 23:06:43 -0700

    updated

---
+++ lintcode/228. Middle of Linked List.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param head: the head of linked list.
+     * @return: a middle node of the linked list
+     */
+    public ListNode middleNode(ListNode head) {
+        // 同向双指针-快慢指针； 
+        // 慢指针每次走一步，快指针每次走两步，当快指针走到头的时候， 慢指针的位置就是中点
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
