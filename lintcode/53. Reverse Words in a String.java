4a38fa2e120ef939f150b5ce472b2bba919a4ae2
Waynexia888 <qiwuwaynexia@gmail.com>
2020-07-20 21:36:10 -0700

    updated

---
+++ lintcode/53. Reverse Words in a String.java
@@ -0,0 +1,28 @@
+public class Solution {
+    /*
+     * @param s: A string
+     * @return: A string
+     */
+    public String reverseWords(String s) {
+        // write your code here
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+        
+        String[] arr = s.split(" ");
+        StringBuilder sb = new StringBuilder(); // 相当于空空字符串
+        // 从后往前遍历数组
+        for (int i = arr.length - 1; i >= 0; i--) {
+            // eg; arr = ["", "hello", "world", ""]
+            if (arr[i].equals("")) {
+                continue;
+            }
+            // 当前字符串不等于""的时候， 且不是模拟器第一个元素的情况下
+            if (sb.length() > 0) {
+                sb.append(" ");
+            }
+            sb.append(arr[i]);
+        }
+        return sb.toString();
+    }
+}
