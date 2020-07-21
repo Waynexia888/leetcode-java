57022668f0f296b699b4455fe26a6fb4bfd4945f
Waynexia888 <qiwuwaynexia@gmail.com>
2020-07-20 21:25:07 -0700

    updated

--- lintcode/13. Implement strStr().java
+++ lintcode/13. Implement strStr().java
@@ -24,3 +24,38 @@ public class Solution {
 
     }
 }
+
+
+/////////////////
+
+public class Solution {
+    /**
+     * @param source: 
+     * @param target: 
+     * @return: return the index
+     */
+    public int strStr(String source, String target) {
+        // corner case， 如果target是空， 返回下标0;
+        if (target == null || target.length() == 0) {
+            return 0;
+        }
+        // corner case: 如果target长度 大于 source的长度
+        if (target.length() > source.length()) {
+            return -1;
+        }
+        
+        for (int i = 0; i < source.length() - target.length() + 1; i++) {
+            int j;
+            for (j = 0; j < target.length(); j++) {
+                //内层循环时，j可以++； 但是i不能++；因此第一个字母相同时，可以通过比较i + j 和 j比较下一个字母是否相同
+                if (source.charAt(i + j) != target.charAt(j)) {
+                    break;
+                }
+            }
+            if (j == target.length()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
