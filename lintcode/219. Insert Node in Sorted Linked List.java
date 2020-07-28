2d7f340f32204cb08b908be479a4d7e29e86db06
Waynexia888 <qiwuwaynexia@gmail.com>
2020-07-28 16:13:23 -0700

    updated

--- lintcode/219. Insert Node in Sorted Linked List.java
+++ lintcode/219. Insert Node in Sorted Linked List.java
@@ -17,18 +17,20 @@ public class Solution {
      * @return: The head of new linked list.
      */
     public ListNode insertNode(ListNode head, int val) {
-        // given: null, val = 5,  5->null,
-        // use a dummy node to begin with, then find the position where the node.val <= val, then insert the node at location + 1
+        // write your code here
         ListNode node = new ListNode(val);
         ListNode dummy = new ListNode(-1);
         dummy.next = head;
-        head = dummy;
-        
-        while (head.next != null && head.next.val <= val) {
-            head = head.next;
+        // 因为要插入的点有可能在head的前面，因此是从dummy点开始
+        // 并且每次都是对dummy.next进行比较，这样能在要插入的位置的前一个位置停下来
+        ListNode current = dummy;
+        while (current.next != null && current.next.val < val) {
+            current = current.next;
         }
-        node.next = head.next;
-        head.next = node;
+        
+        // 找到合适的位置后停下来
+        node.next = current.next;
+        current.next = node;
         
         return dummy.next;
     }
