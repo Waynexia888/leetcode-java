ee9b36123e8b6a1bee9fb2ccd9983adecf6d0e50
Waynexia888 <qiwuwaynexia@gmail.com>
2020-08-01 16:54:40 -0700

    updated

--- lintcode/433. Number of Islands.java
+++ lintcode/433. Number of Islands.java
@@ -59,3 +59,59 @@ public class Solution {
         return false;
     }
 }
+
+////////////////////////////////////////////////////////
+public class Solution {
+    /**
+     * @param grid: a boolean 2D matrix
+     * @return: an integer
+     */
+    public int numIslands(boolean[][] grid) {
+        // DFS 
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        
+        int[][] DIRECTIONS = {
+            {0, 1},
+            {0, -1},
+            {1, 0},
+            {-1, 0}
+        };
+        
+        int rows = grid.length;
+        int columns = grid[0].length;
+        boolean[][] visited = new boolean[rows][columns];
+        
+        int numberOfIsland = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (check(grid, i, j, visited)) {
+                    dfs(grid, i, j, visited, DIRECTIONS);
+                    numberOfIsland++;
+                }
+            }
+        }
+        return numberOfIsland;
+    }
+    
+    private void dfs(boolean[][] grid, int x, int y, boolean[][] visited, int[][] DIRECTIONS) {
+        visited[x][y] = true;
+        for (int k = 0; k < 4; k++) {
+            int nx = x + DIRECTIONS[k][0];
+            int ny = y + DIRECTIONS[k][1];
+            if (check(grid, nx, ny, visited)) {
+                dfs(grid, nx, ny, visited, DIRECTIONS);
+            }
+        }
+    }
+    
+    private boolean check(boolean[][] grid, int x, int y, boolean[][] visited) {
+        int rows = grid.length;
+        int columns = grid[0].length;
+        if (x >= 0 && x < rows && y >= 0 && y < columns && grid[x][y] && !visited[x][y]) {
+            return true;
+        }
+        return false;
+    }
+}
