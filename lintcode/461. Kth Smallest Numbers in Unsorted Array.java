3d6e6d7340227507147a09c98f6804d7501a8e95
Waynexia888 <qiwuwaynexia@gmail.com>
2020-07-31 22:41:10 -0700

    updated

---
+++ lintcode/461. Kth Smallest Numbers in Unsorted Array.java
@@ -0,0 +1,66 @@
+public class Solution {
+    /**
+     * @param k: An integer
+     * @param nums: An integer array
+     * @return: kth smallest element
+     */
+    public int kthSmallest(int k, int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        return quickselect(k, nums, 0, nums.length - 1);
+    }
+    
+    private int quickselect(int k, int[] nums, int start, int end) {
+        if (start == end) {
+            return nums[start];
+        }
+        
+        int left = start, right = end;
+        int pivot = nums[(start + end) / 2];
+        while (left <= right) {
+            while (left <= right && nums[left] < pivot) {
+                left++;
+            }
+            while (left <= right && nums[right] > pivot) {
+                right--;
+            }
+            if (left <= right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        
+        // from smallest to largest
+        // right > left
+        // [start, ..., right, x, left, ... end]
+        if (start + k - 1 <= right) {
+            return quickselect(k, nums, start, right);
+        }
+        if (start + k - 1 >= left) {
+            return quickselect(k - (left - start), nums, left, end);
+        }
+        return nums[right + 1];
+        
+    }
+}
+
+// [3, 4, 1, 2, 5] pivot = 1 
+//  L           R 
+ 
+// [3, 4, 1, 2, 5] 
+//  L     R 
+ 
+
+// [1, 4, 3, 2, 5] 
+//     L  
+//     R 
+    
+// [1, 4, 3, 2, 5] 
+//     L  
+//  R 
