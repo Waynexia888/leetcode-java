75048bbe44a59d956481ca2c852f6cae1990f15f
Waynexia888 <qiwuwaynexia@gmail.com>
2020-08-26 16:58:43 -0700

    updated

--- amazon高频题/1324. Count Primes.java
+++ amazon高频题/1324. Count Primes.java
@@ -6,29 +6,26 @@ public class Solution {
     public int countPrimes(int n) {
         // prime number从2 开始;
         // prime number的定义是一个正整数只能被1和本身整除。
-        // A prime number is a number which is divisible by only two numbers: 1 and
-        // itself. So, if any number is divisible by any other number, it is not a prime
-        // number.
+        // A prime number is a number which is divisible by only two numbers: 1 and itself. So, if any number is divisible by any other number, it is not a prime number.
         // 时间复杂度：O(n) * O(sqrt(n)), 空间复杂度O(n)
-
+        
         boolean[] isPrime = new boolean[n];
         Arrays.fill(isPrime, true);
-
-        // n = 12, 12 = 2 * 6; 12 = 3 * 4; 12 = sqrd(12) * sqrd(12); 12 = 4 * 3; 12 = 6
-        // * 2;
-        // 如果在[2, sqrt(n)]这个区间内没有发现整除因子， 那么在[sqrt(n), n]也一定不会发现整除因子
+        
+        // n = 12, 12 = 2 * 6; 12 = 3 * 4; 12 = sqrd(12) * sqrd(12); 12 = 4 * 3; 12 = 6 * 2;
+        //如果在[2, sqrt(n)]这个区间内没有发现整除因子， 那么在[sqrt(n), n]也一定不会发现整除因子
         for (int i = 2; i * i < n; i++) {
             if (isPrime[i]) {
                 // i 的倍数不可能是质数
                 // i = 2, 2 * 2; 4 + 2； 6 + 2； 8 + 2；
-                // i = 3; 2 * 3; 6 + 3;
+                // i = 3; 2 * 3; 6 + 3; 
                 // 2, 3, 4false, 5, 6false, 7, 8false, 9false, 10false, 11
                 for (int j = 2 * i; j < n; j += i) {
                     isPrime[j] = false;
                 }
             }
         }
-
+        
         int count = 0;
         for (int i = 2; i < n; i++) {
             if (isPrime[i]) {
@@ -37,5 +34,6 @@ public class Solution {
         }
         return count;
     }
-
+    
+    
 }
---
+++ amazon高频题/167. Add Two Numbers.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param l1: the first list
+     * @param l2: the second list
+     * @return: the sum list of l1 and l2 
+     */
+    public ListNode addLists(ListNode l1, ListNode l2) {
+        // write your code here
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        ListNode p = l1, q = l2;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            tail.next = new ListNode(sum % 10);
+            tail = tail.next;
+            if (p != null) {
+                p = p.next;
+            }
+            
+            if (q != null) {
+                q = q.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return dummy.next;
+    }
+}
