8dd34cefc9d6b2f8daff43ec058373dffd986d1b
Waynexia888 <qiwuwaynexia@gmail.com>
2020-08-27 17:18:49 -0700

    updated

--- lintcode/双指针 Two Pointers/滑动窗口类指针/406. Minimum Size Subarray Sum.java
+++ lintcode/双指针 Two Pointers/滑动窗口类指针/406. Minimum Size Subarray Sum.java
@@ -37,3 +37,39 @@ public class Minimum_Size_Subarray_Sum {
 
     }
 }
+
+///////////////////////////////////////////////////
+public class Solution {
+    /**
+     * @param nums: an array of integers
+     * @param s: An integer
+     * @return: an integer representing the minimum size of subarray
+     */
+    public int minimumSize(int[] nums, int s) {
+        // 滑动窗口型指针移动题型
+        // time：O(n), 尽管两层循环， 但是j一直往前走，没有返回
+        // space: O(1)
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        int i = 0, j = 0;
+        int sum = 0;
+        int ans = Integer.MAX_VALUE;
+        for (i = 0; i < nums.length; i++) {
+            while (j < nums.length && sum < s) {
+                sum += nums[j];
+                j++;
+            }
+            if (sum >= s) {
+                ans = Math.min(ans, j - i);
+            }
+            sum -= nums[i];
+        }
+        
+        if (ans == Integer.MAX_VALUE) {
+            ans = -1;
+        }
+        return ans;
+    }
+}
