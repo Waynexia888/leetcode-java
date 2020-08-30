5ca32fb47790fd113994e52efb8eb567eebcc047
Waynexia888 <qiwuwaynexia@gmail.com>
2020-08-29 17:16:10 -0700

    updated

---
+++ Leetcode刷刷刷/Arrays/排序算法 Sorting/493. Reverse Pairs.java
@@ -0,0 +1,22 @@
+// Given an array nums, we call (i, j) an important reverse pair if i < j and 
+// nums[i] > 2*nums[j].
+
+// You need to return the number of important reverse pairs in the given array.
+
+// Example1:
+
+// Input: [1,3,2,3,1]
+// Output: 2
+// Example2:
+
+// Input: [2,4,3,5,1]
+// Output: 3
+// Note:
+// 1. The length of the given array will not exceed 50,000.
+// 2. All the numbers in the input array are in the range of 32-bit integer.
+
+// 本题思路 - 归并排序
+// 难度 hrad， 近6个月，ByteDance考过6次，其余公司没有
+// 暂且放过，日后再做
+
+// 同类型题： leetcode 315， leetcode 327
