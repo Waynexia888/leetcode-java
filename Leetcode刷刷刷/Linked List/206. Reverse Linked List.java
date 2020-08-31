db178a891eb1307d03a10074a424c13cd5e7dc8a
Waynexia888 <qiwuwaynexia@gmail.com>
2020-08-30 19:28:40 -0700

    updated

---
+++ Leetcode刷刷刷/Linked List/206. Reverse Linked List.java
@@ -0,0 +1,43 @@
+// Reverse a singly linked list.
+
+// Example:
+
+// Input: 1->2->3->4->5->NULL
+// Output: 5->4->3->2->1->NULL
+// Follow up:
+
+// A linked list can be reversed either iteratively or recursively. 
+// Could you implement both?
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        ListNode pre = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = pre;
+            pre = head;
+            head = temp;
+        }
+        return pre;
+    }
+}
+
+// 本题思想： 链表的翻转
+// 需要理解背诵
+// 时间复杂度: O(n)
+// 空间复杂度: O(1)
