a94d6088646388cf196db0c9f507bb89bbbe98fd
Waynexia888 <qiwuwaynexia@gmail.com>
2020-11-09 21:02:21 -0800

    updated

--- Leetcode刷刷刷/Linked List/环/142. Linked List Cycle II.java
+++ Leetcode刷刷刷/Linked List/环/142. Linked List Cycle II.java
@@ -93,3 +93,43 @@ public class Solution {
         return null;
     }
 }
+
+//////////////////////////////////////////////
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        // time: O(n); space: O(1)
+        if (head == null || head.next == null) {
+            return null;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            // the first time when slow and fast pointer meet together
+            if (slow == fast) {
+                // we've proved that a = c; let slow and slow2 pointer move one space every time.
+                ListNode slow2 = head;
+                while (slow != slow2) {
+                    slow = slow.next;
+                    slow2 = slow2.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
