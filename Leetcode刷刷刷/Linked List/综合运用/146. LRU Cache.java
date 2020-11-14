4cf8c16cf636cbf17a482773ee67946350b9fc15
Waynexia888 <qiwuwaynexia@gmail.com>
2020-11-14 02:25:15 -0800

    updated

--- Leetcode刷刷刷/Linked List/综合运用/146. LRU Cache.java
+++ Leetcode刷刷刷/Linked List/综合运用/146. LRU Cache.java
@@ -1,18 +1,22 @@
 class LRUCache {
+    
+    // use linked list data structure
     class ListNode {
         public int key, val;
         public ListNode next;
-        
         public ListNode(int key, int val) {
             this.key = key;
             this.val = val;
-            this.next = null;   
+            this.next = null;
         }
     }
+    
+    public int capacity, size;
+    public ListNode dummy;
+    public ListNode tail;
+    // use hashmap data structure; key: the key of current node; value: previous node of current node
+    public Map<Integer, ListNode> keyToPrev;
 
-    private int capacity, size;
-    private ListNode dummy, tail;
-    private Map<Integer, ListNode> keyToPrev;
     public LRUCache(int capacity) {
         this.capacity = capacity;
         this.size = size;
@@ -22,34 +26,36 @@ class LRUCache {
     }
     
     public int get(int key) {
-        // if key not exist
+        // key is not exist
         if (!keyToPrev.containsKey(key)) {
             return -1;
         }
         
-        // key exist
+        // key is exist
         moveToTail(key);
         return tail.val;
     }
     
     public void put(int key, int value) {
-        // key exist, update the value
+        // key exist, update its value
         if (get(key) != -1) {
             tail.val = value;
             return;
         }
         
         // key not exist, and the capacity is not full
+        //        add a new node to the end of the list
         if (size < capacity) {
             size++;
-            ListNode node = new ListNode(key, value);
-            tail.next = node;
+            ListNode newNode = new ListNode(key, value);
+            tail.next = newNode;
             keyToPrev.put(key, tail);
             tail = tail.next;
-            return;
+            return;  
         }
         
         // key not exist, and the capacity is full
+        //        remove the old first node, and add a new node in the end
         ListNode first = dummy.next;
         keyToPrev.remove(first.key);
         
@@ -59,21 +65,26 @@ class LRUCache {
         moveToTail(first.key);
     }
     
-    public void moveToTail(int key) {
+    private void moveToTail(int key) {
+        // previous node 
         ListNode prev = keyToPrev.get(key);
-        ListNode temp = prev.next;
+        ListNode currentNode = prev.next;
         
-        if (temp == tail) {
+        // check if currentNode is in the end of the list
+        if (currentNode == tail) {
             return;
         }
         
-        prev.next = temp.next;
-        tail.next = temp;
-        temp.next = null;
+        // otherwise, re-construct the linked list
+        prev.next = currentNode.next;
+        tail.next = currentNode;
+        currentNode.next = null;
         
+        // update the hashmap
         keyToPrev.put(prev.next.key, prev);
-        keyToPrev.put(temp.key, tail);
+        keyToPrev.put(currentNode.key, tail);
         
+        // update the tail
         tail = tail.next;
     }
 }
@@ -85,6 +96,9 @@ class LRUCache {
  * obj.put(key,value);
  */
 
-// hashmap + Linked List
-// hashmap: key: key, which is an Integer; value: previous node, which is a ListNode
+// why LinkedList?
+//     use O(1) time complexity to insert and delete one element if we know the previous node
+    
+// why HashMap?
+//     insertion, deletion, and search takes O(1) constant time. 
 // time: O(1); space: O(capacity)
