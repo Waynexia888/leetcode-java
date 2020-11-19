ff02f23a2b59878a587ad41e7758a9d39a1240db
Waynexia888 <qiwuwaynexia@gmail.com>
2020-11-18 19:28:26 -0800

    updated

---
+++ Leetcode刷刷刷/String/String: 基础 & 实现/168. Excel Sheet Column Title.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String convertToTitle(int n) {
+        // time: O(log(n)); space: O(log(n))
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            n--;
+            sb.append((char)('A' + n % 26));
+            n /= 26;
+        }
+        return sb.reverse().toString();
+    }
+}
