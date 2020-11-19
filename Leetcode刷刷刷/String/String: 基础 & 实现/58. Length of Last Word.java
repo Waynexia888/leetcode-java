ca050dce4658d1f0355a68d521da274e47ecbc70
Waynexia888 <qiwuwaynexia@gmail.com>
2020-11-18 18:52:54 -0800

    updated

---
+++ Leetcode刷刷刷/String/String: 基础 & 实现/58. Length of Last Word.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String[] str = s.split(" ");
+        if (str.length == 0) {
+            return 0;
+        }
+        return str[str.length - 1].length();
+    }
+}
