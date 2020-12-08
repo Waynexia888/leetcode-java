e31e8939912a89ddadf19ba51a3be1cf946c213b
Waynexia888 <qiwuwaynexia@gmail.com>
2020-12-08 14:11:05 -0800

    updated

--- Leetcode刷刷刷/String/String: 基础 & 实现/482. License Key Formatting.java
+++ Leetcode刷刷刷/String/String: 基础 & 实现/482. License Key Formatting.java
@@ -12,3 +12,22 @@ class Solution {
         return sb.reverse().toString().toUpperCase();
     }
 }
+
+
+/////////////
+class Solution {
+    public String licenseKeyFormatting(String S, int K) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = S.length() - 1; i >= 0; i--) {
+            if (S.charAt(i) != '-') {
+                if (sb.length() % (K + 1) != K) {
+                    sb.append(S.charAt(i));
+                } else {
+                    sb.append('-');
+                    sb.append(S.charAt(i));
+                }
+            }
+        }
+        return sb.reverse().toString().toUpperCase();
+    }
+}
