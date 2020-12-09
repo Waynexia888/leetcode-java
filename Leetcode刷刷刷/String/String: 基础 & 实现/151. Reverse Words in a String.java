5145eb41ad6b622380d12fce5ecc5f7874a3ba9f
Waynexia888 <qiwuwaynexia@gmail.com>
2020-12-09 12:32:52 -0800

    updated

--- Leetcode刷刷刷/String/String: 基础 & 实现/151. Reverse Words in a String.java
+++ Leetcode刷刷刷/String/String: 基础 & 实现/151. Reverse Words in a String.java
@@ -15,3 +15,33 @@ class Solution {
         return sb.toString().trim();
     }
 }
+
+////////////////////////
+
+class Solution {
+    public String reverseWords(String s) {
+        s = s.trim();
+        String[] arr = s.split("\\s+");
+        
+        // 相向双指针，reverse的步骤
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            String temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        
+        // 最后， 如何将String[] 转换成String呢？
+        String str = "";  //创建一个空字符串
+        for (int i = 0; i < arr.length; i++) {
+            if (i != arr.length - 1) {
+                str += arr[i] + " ";
+            } else {
+                str += arr[i];
+            } 
+        }
+        return str;
+    }
+}
