661eb84b614ec84db684154a482a36bfe9bd8bbb
Waynexia888 <qiwuwaynexia@gmail.com>
2020-12-10 12:09:12 -0800

    updated

---
+++ Leetcode刷刷刷/String/String: 基础 & 实现/551. Student Attendance Record I.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        int countA = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'A') {
+                countA++;
+            }
+            if (i <= s.length() - 3 && s.charAt(i) == 'L' && s.charAt(i + 1) == 'L' && s.charAt(i + 2) == 'L') {
+                return false;
+            }
+        }
+        if (countA > 1) return false;
+        return true;
+    }
+}
+
+Time： O(n); space: O(1)
