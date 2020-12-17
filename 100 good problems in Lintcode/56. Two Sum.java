efc66a716f1e7ecaee89c57020f8622ca3261f77
Waynexia888 <qiwuwaynexia@gmail.com>
2020-12-17 12:57:57 -0800

    updated

--- 100 good problems in Lintcode/56. Two Sum.java
+++ 100 good problems in Lintcode/56. Two Sum.java
@@ -21,3 +21,35 @@ public class Solution {
         return new int[0]; // 长度为0的， 空数组
     }
 }
+
+
+///////////////////////////////////////////////////////////
+
+public class Solution {
+    /**
+     * @param numbers: An array of Integer
+     * @param target: target = numbers[index1] + numbers[index2]
+     * @return: [index1 + 1, index2 + 1] (index1 < index2)
+     */
+    public int[] twoSum(int[] numbers, int target) {
+        // write your code here
+        if (numbers == null || numbers.length == 0) {
+            return new int[]{0, 0};
+        }
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            int num = target - numbers[i];
+            if (!map.containsKey(num)) {
+                map.put(numbers[i], i);
+            } else {
+                result[0] = map.get(num);
+                result[1] = i;
+            }
+        }
+        return result;
+    }
+}
+
+time: O(n);
+space: O(n)
