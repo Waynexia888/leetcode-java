882a65cf5f52848e9b2c38952731a5985e1d71f9
Waynexia888 <qiwuwaynexia@gmail.com>
2020-12-29 13:45:09 -0800

    updated

--- lintcode/228. Middle of Linked List.java
+++ lintcode/228. Middle of Linked List.java
@@ -31,3 +31,28 @@ public class Solution {
         return slow;
     }
 }
+
+
+////////////////
+
+public class Solution {
+    /**
+     * @param head: the head of linked list.
+     * @return: a middle node of the linked list
+     */
+    public ListNode middleNode(ListNode head) {
+        // write your code here
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
