db38b6765090ed6a02c93eb29d55bb0a9174f5cd
Waynexia888 <qiwuwaynexia@gmail.com>
2021-01-09 19:09:42 -0800

    updated

--- lintcode/队列和栈 Queue and Stack/492. Implement Queue by Linked List.java
+++ lintcode/队列和栈 Queue and Stack/492. Implement Queue by Linked List.java
@@ -1,33 +1,19 @@
-class Node {
-    public int val;
-    public Node next;
-
-    public Node(int val) {
-        this.val = val;
-        next = null;
-    }
-}
-
 public class MyQueue {
     /*
      * @param item: An integer
-     * 
      * @return: nothing
      */
-    public Node head, tail;
-
+    public ListNode head, tail;
     public MyQueue() {
         head = tail = null;
     }
-
     public void enqueue(int item) {
         // write your code here
-
         if (head == null) {
-            tail = new Node(item);
+            tail = new ListNode(item);
             head = tail;
         } else {
-            tail.next = new Node(item);
+            tail.next = new ListNode(item);
             tail = tail.next;
         }
     }
@@ -37,12 +23,23 @@ public class MyQueue {
      */
     public int dequeue() {
         // write your code here
-        if (head == null) {
-            return -1;
-        } else {
-            int item = head.val;
+        if (head != null) {
+            int val = head.val;
             head = head.next;
-            return item;
+            return val;
         }
+        return -1;
     }
 }
+
+class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+enqueue() -> O(1)
+dequeue() -> O(1)
