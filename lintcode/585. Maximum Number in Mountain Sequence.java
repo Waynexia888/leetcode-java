aee26840cd990acc24b77b5c58a863cdff1f2d19
Waynexia888 <qiwuwaynexia@gmail.com>
2021-02-03 15:20:17 -0800

    Lintcode 585

---
+++ lintcode/585. Maximum Number in Mountain Sequence.java
@@ -0,0 +1,25 @@
+public class Solution {
+    /**
+     * @param nums: a mountain sequence which increase firstly and then decrease
+     * @return: then mountain top
+     */
+    public int mountainSequence(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        // find first index i so that nums[i] > nums[i + 1]
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        
+        return Math.max(nums[start], nums[end]);
+    }
+}
