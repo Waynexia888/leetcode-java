805589dd4f8bf991709a78715188e82ae9b8e32e
Waynexia888 <qiwuwaynexia@gmail.com>
2021-02-03 17:29:51 -0800

    Lintcode 159

---
+++ lintcode/159. Find Minimum in Rotated Sorted Array.java
@@ -0,0 +1,23 @@
+public class Solution {
+    /**
+     * @param nums: a rotated sorted array
+     * @return: the minimum number in the array
+     */
+    public int findMin(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] <= nums[end]) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        return Math.min(nums[start], nums[end]);
+    }
+}
