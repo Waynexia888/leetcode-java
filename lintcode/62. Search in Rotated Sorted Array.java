607d6e84241e84d42d7ca25cdebafa4edf3b5213
Waynexia888 <qiwuwaynexia@gmail.com>
2021-02-04 14:32:32 -0800

    Lintcode 62 METHOD 2

--- lintcode/62. Search in Rotated Sorted Array.java
+++ lintcode/62. Search in Rotated Sorted Array.java
@@ -60,3 +60,48 @@ public class Solution {
 }
 
 time:O(logn); space: O(1)
+
+//////////////////////////////////////////////
+
+public class Solution {
+    /**
+     * @param A: an integer rotated sorted array
+     * @param target: an integer to be searched
+     * @return: an integer
+     */
+    public int search(int[] A, int target) {
+        // write your code here
+        if (A == null || A.length == 0) {
+            return -1;
+        }
+        
+        int start = 0, end = A.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] >= A[start]) {
+                if (A[start] <= target && target <= A[mid]) {
+                    end = mid;
+                } else {
+                    start = mid;
+                }
+            } else {
+                if (target > A[mid] && target <= A[end]) {
+                    start = mid;
+                } else {
+                    end = mid;
+                }
+            }
+        }
+        
+        if (A[start] == target) {
+            return start;
+        }
+        
+        if (A[end] == target) {
+            return end;
+        }
+        
+        return -1;
+    }
+}
+
