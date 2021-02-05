fc58c7607f0778e844ccd1faee63f12f638faa19
Waynexia888 <qiwuwaynexia@gmail.com>
2021-02-04 17:35:11 -0800

    Lintcode 28

---
+++ lintcode/28. Search a 2D Matrix.java
@@ -0,0 +1,34 @@
+public class Solution {
+    /**
+     * @param matrix: matrix, a list of lists of integers
+     * @param target: An integer
+     * @return: a boolean, indicate whether matrix contains target
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // write your code here
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        
+        int m = matrix.length, n = matrix[0].length;
+        int start = 0, end = m * n - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (matrix[mid / n][mid % n] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        if (matrix[start / n][start % n] == target) {
+            return true;
+        }
+        
+        if (matrix[end / n][end % n] == target) {
+            return true;
+        }
+        return false;
+    }
+}
+
