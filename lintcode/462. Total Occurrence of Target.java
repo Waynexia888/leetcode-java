ca9fba47100166f11b1488b7dd440a465ef05d78
Waynexia888 <qiwuwaynexia@gmail.com>
2021-02-08 12:31:11 -0800

    Lintcode 462

---
+++ lintcode/462. Total Occurrence of Target.java
@@ -0,0 +1,62 @@
+public class Solution {
+    /**
+     * @param A: A an integer array sorted in ascending order
+     * @param target: An integer
+     * @return: An integer
+     */
+    public int totalOccurrence(int[] A, int target) {
+        // write your code here
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        
+        int last = findLastPositionOfTarget(A, target);
+        int first = findFisrtPositionOfTarget(A, target);
+        if (first == -1 && last == -1) {
+            return 0;
+        }
+        return last - first + 1;
+    }
+    
+    private int findLastPositionOfTarget(int[] A, int target) {
+        int start = 0, end = A.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] <= target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        if (A[end] == target) {
+            return end;
+        }
+        
+        if (A[start] == target) {
+            return start;
+        }
+        return -1;
+    }
+    
+    private int findFisrtPositionOfTarget(int[] A, int target) {
+        int start = 0, end = A.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] >= target) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        
+        if (A[start] == target) {
+            return start;
+        }
+        
+        if (A[end] == target) {
+            return end;
+        }
+        return -1;
+    }
+}
