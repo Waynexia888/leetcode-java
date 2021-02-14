a3a6fe79aa2f8b56c0cde2cc0755a53fefaa90ca
Waynexia888 <qiwuwaynexia@gmail.com>
2021-02-14 11:33:49 -0800

    Lintcode 607

--- lintcode/607. Two Sum III - Data structure design.java
+++ lintcode/607. Two Sum III - Data structure design.java
@@ -33,3 +33,37 @@ public class TwoSum {
         return false;
     }
 }
+
+////////////////////////////////////
+public class TwoSum {
+    /**
+     * @param number: An integer
+     * @return: nothing
+     */
+     
+    public Map<Integer, Integer> map = new HashMap<>();
+    public void add(int number) {
+        // write your code here
+        int count = map.getOrDefault(number, 0);
+        map.put(number, count + 1);
+    }
+
+    /**
+     * @param value: An integer
+     * @return: Find if there exists any pair of numbers which sum is equal to the value.
+     */
+    // two sum, num1 + num2 ??? value
+    public boolean find(int value) {
+        // write your code here
+        for (Integer num : map.keySet()) {
+            int num1 = value - num;
+            if (num1 == num && map.get(num) >= 2) {
+                return true;
+            }
+            if (num1 != num && map.containsKey(num1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
