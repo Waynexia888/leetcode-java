87c42f8a02e3c663589e7b0e8e670a8b1d57ca22
Waynexia888 <qiwuwaynexia@gmail.com>
2021-02-16 02:17:45 -0800

    Lintcode 58 second method

--- lintcode/58. 4Sum.java
+++ lintcode/58. 4Sum.java
@@ -49,3 +49,61 @@ public class Solution {
         return result;
     }
 }
+
+////////////////////////////////////////////////
+
+public class Solution {
+    /**
+     * @param numbers: Give an array
+     * @param target: An integer
+     * @return: Find all unique quadruplets in the array which gives the sum of zero
+     */
+    public List<List<Integer>> fourSum(int[] numbers, int target) {
+        // write your code here
+        List<List<Integer>> result = new ArrayList<>();
+        if (numbers == null || numbers.length < 4) {
+            return result;
+        }
+        
+        Arrays.sort(numbers);
+        for (int i = 0; i < numbers.length - 3; i++) {
+            if (i != 0 && numbers[i] == numbers[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < numbers.length - 2; j++) {
+                if (j != i + 1 && numbers[j] == numbers[j - 1]) {
+                    continue;
+                }
+                int left = j + 1, right = numbers.length - 1;
+                twoSum(numbers, numbers[i], numbers[j], left, right, target, result);
+            }
+        }
+        return result;
+    }
+    
+    private void twoSum(int[] numbers, int firstNumber, int SecondNumber, int left, int right, int target, List<List<Integer>> result) {
+        while (left < right) {
+            int sum = firstNumber + SecondNumber + numbers[left] + numbers[right];
+            if (sum == target) {
+                List<Integer> temp = new ArrayList<>();
+                temp.add(firstNumber);
+                temp.add(SecondNumber);
+                temp.add(numbers[left]);
+                temp.add(numbers[right]);
+                result.add(temp);
+                left++;
+                right--;
+                while (left < right && numbers[left] == numbers[left - 1]) {
+                    left++;
+                }
+                while (left < right && numbers[right] == numbers[right + 1]) {
+                    right--;
+                }
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+}
