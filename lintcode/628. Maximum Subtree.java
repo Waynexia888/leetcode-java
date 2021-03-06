e5dd8d79d32d7563b154e5084741d4c6bf8428fe
Waynexia888 <qiwuwaynexia@gmail.com>
2021-03-05 20:47:38 -0800

    Lintcode 628

--- lintcode/628. Maximum Subtree.java
+++ lintcode/628. Maximum Subtree.java
@@ -36,3 +36,48 @@ public class Solution {
         return root_weight;
     }
 }
+
+////////////////////////////////////////////////////////////////
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: the root of binary tree
+     * @return: the maximum weight node
+     */
+    public TreeNode findSubtree(TreeNode root) {
+        // write your code here
+        TreeNode[] result = new TreeNode[1];
+        int[] maxSum = new int[1];
+        maxSum[0] = Integer.MIN_VALUE;
+        divideConquer(root, result, maxSum);
+        return result[0];
+    }
+
+    private int divideConquer(TreeNode root, TreeNode[] result, int[] maxSum) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = divideConquer(root.left, result, maxSum);
+        int right = divideConquer(root.right, result, maxSum);
+        int sum = left + right + root.val;
+
+        if (sum > maxSum[0]) {
+            maxSum[0] = sum;
+            result[0] = root;
+        }
+        return sum;
+    }
+}
