32aac43067e58bf87e904c523c98e506f802af33
Waynexia888 <qiwuwaynexia@gmail.com>
2021-03-11 19:47:14 -0800

    Lintcode 433

--- lintcode/433. Number of Islands.java
+++ lintcode/433. Number of Islands.java
@@ -115,3 +115,84 @@ public class Solution {
         return false;
     }
 }
+
+////////////////////////////////////////////////////////////
+class Point {
+    int x, y;
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Solution {
+    /**
+     * @param grid: a boolean 2D matrix
+     * @return: an integer
+     */
+    public int numIslands(boolean[][] grid) {
+        // 遍历这个二维矩阵
+        //     如果这个点为岛屿，且没有被访问过
+        //         从这个点开始bfs，找到相邻的所有的岛屿，然后把它们标记访问过
+        // 返回大岛屿的数量
+
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+
+        int rows = grid.length;
+        int columns = grid[0].length;
+        int number = 0;
+        boolean[][] visited = new boolean[rows][columns]; 
+        
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (grid[i][j] && visited[i][j] == false) {
+                    number++;
+                    bfs(grid, i, j, visited);
+                }
+            }
+        }
+        return number;
+    }
+
+    private void bfs(boolean[][] grid, int i, int j, boolean[][] visited) {
+        int[] dx = new int[]{0, 0, 1, -1};
+        int[] dy = new int[]{1, -1, 0, 0};
+
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(new Point(i, j));
+        visited[i][j] = true;
+
+        while (!queue.isEmpty()) {
+            Point point = queue.poll();
+
+            for (int k = 0; k < 4; k++) {
+                int nx = point.x + dx[k];
+                int ny = point.y + dy[k];
+                if (isValid(grid, nx, ny, visited)) {
+                    queue.add(new Point(nx, ny));
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+    }
+
+    private boolean isValid(boolean[][] grid, int nx, int ny, boolean[][] visited) {
+        // 判断（nx, ny)是否在矩阵里，是否是一个岛屿， 没有被访问过
+        int rows = grid.length;
+        int columns = grid[0].length;
+        if (nx < 0 || nx >= rows || ny < 0 || ny >= columns) {
+            return false;
+        }
+        if (!grid[nx][ny]) {
+            return false;
+        }
+        if (visited[nx][ny] == true) {
+            return false;
+        }
+        return true;
+    }
+}
+
