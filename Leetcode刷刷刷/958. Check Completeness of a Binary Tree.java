be70214cf570f0525c5340958b717c1dfc95749f
Waynexia888 <qiwuwaynexia@gmail.com>
2021-03-15 01:12:06 -0700

    Leetcode 958 Method 1

---
+++ Leetcode刷刷刷/958. Check Completeness of a Binary Tree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCompleteTree(TreeNode root) {
+        if (root == null) return true;
+        
+        boolean hasNull = false;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node == null) { // node为空的情况
+                    hasNull = true;
+                } else {            // node不为空的情况
+                    if (hasNull) return false;
+                    queue.offer(node.left);
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return true;
+    }
+}
