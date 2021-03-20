af2f58bf692733a60b2e6af1f73b59cefaa7adb1
Waynexia888 <qiwuwaynexia@gmail.com>
2021-03-19 20:07:42 -0700

    Lintcode 127

--- lintcode/127. Topological Sorting.java
+++ lintcode/127. Topological Sorting.java
@@ -69,3 +69,60 @@ public class Solution {
     }
     
 }
+
+//////////////////////////////////////////////////////////////
+/**
+ * Definition for Directed graph.
+ * class DirectedGraphNode {
+ *     int label;
+ *     ArrayList<DirectedGraphNode> neighbors;
+ *     DirectedGraphNode(int x) { label = x; neighbors = new ArrayList<DirectedGraphNode>(); }
+ * };
+ */
+
+public class Solution {
+    /*
+     * @param graph: A list of Directed graph node
+     * @return: Any topological order for the given graph.
+     */
+    public ArrayList<DirectedGraphNode> topSort(ArrayList<DirectedGraphNode> graph) {
+        // write your code here
+        ArrayList<DirectedGraphNode> results = new ArrayList<>();
+        if (graph == null) {
+            return results;
+        }
+
+        // 1. use HashMap tp count in-degree
+        Map<DirectedGraphNode, Integer> map = new HashMap<>();
+        for (DirectedGraphNode node : graph) {
+            for (DirectedGraphNode neighbor : node.neighbors) {
+                if (map.containsKey(neighbor)) {
+                    map.put(neighbor, map.get(neighbor) + 1);
+                } else {
+                    map.put(neighbor, 1);
+                }
+            }
+        }
+
+        // 2. find out nodes that has 0-indegree, and put it into queue
+        Queue<DirectedGraphNode> queue = new LinkedList<>();
+        for (DirectedGraphNode node : graph) {
+            if (!map.containsKey(node)) {
+                queue.offer(node);
+            }
+        }
+
+        // 3. bfs
+        while (!queue.isEmpty()) {
+            DirectedGraphNode node = queue.poll();
+            results.add(node);
+            for (DirectedGraphNode nei : node.neighbors) {
+                map.put(nei, map.get(nei) - 1);
+                if (map.get(nei) == 0) {
+                    queue.offer(nei);
+                }
+            }
+        }
+        return results;
+    }
+}
