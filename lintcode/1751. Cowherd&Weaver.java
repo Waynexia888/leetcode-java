0b2a4a57493bd21efff6a071bab5466152032483
Waynexia888 <qiwuwaynexia@gmail.com>
2021-03-29 20:37:01 -0700

    Lintcode 1751

--- lintcode/1751. Cowherd&Weaver.java
+++ lintcode/1751. Cowherd&Weaver.java
@@ -33,3 +33,49 @@
 // It is impossible for them to reunion
 // Notice
 // 2<=n,m<=1000
+
+class Point {
+    int x, y;
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public boolean findHer(String[] maze) {
+    int[] dx = new int[]{0, 0, -1, 1};
+    int[] dy = new int[]{1, -1, 0, 0};
+
+    int rows = maze.length;
+    int columns = maze[0].length();
+
+
+    Queue<Point> queue = new LinkedList<>();
+    boolean[][] visited = new boolean[rows][columns];
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            if (maze[i].charAt(j) == "S") {
+                queue.offer(new Point(i, j));
+                visited[i][j] = true;
+            }
+        }
+    }
+
+    while (!queue.isEmpty()) {
+        Point point = queue.poll();
+        for (int k = 0; k < 4; k++) {
+            int nx = point.x + dx[k];
+            int ny = point.y + dy[k];
+            if (maze[nx].charAt(ny) == "T") {
+                return true;
+            }
+
+            if (nx >= 0 && nx < rows && ny >= 0 && ny < columns && maze[nx].charAt(ny) == "." && !visited[nx][ny]) {
+                queue.offer(new Point(nx, ny));
+                visited[nx][ny] = true;
+            }
+        }
+    }
+    return false;
+}
