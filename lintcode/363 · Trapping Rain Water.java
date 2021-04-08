ea258a5ea3a4a60441fca89619db300aef6ecd43
Waynexia888 <qiwuwaynexia@gmail.com>
2021-04-07 21:18:34 -0700

    Lintcode 669

--- lintcode/363 · Trapping Rain Water.java
+++ lintcode/363 · Trapping Rain Water.java
@@ -39,3 +39,40 @@ public class Solution {
     }
 }
 
+/////////////////////////////////
+public class Solution {
+    /**
+     * @param heights: a list of integers
+     * @return: a integer
+     */
+    public int trapRainWater(int[] heights) {
+        // time: O(n); space: O(1)
+        if (heights == null || heights.length < 2) {
+            return 0;
+        }
+
+        int maxL = heights[0];
+        int maxR = heights[heights.length - 1];
+        int water = 0;
+        int left = 1;
+        int right = heights.length - 2;
+        while (left <= right) {
+            if (maxL < maxR) {
+                if (heights[left] > maxL) {
+                    maxL = heights[left];
+                } else {
+                    water += maxL - heights[left];
+                }
+                left++;
+            } else {
+                if (heights[right] > maxR) {
+                    maxR = heights[right];
+                } else {
+                    water += maxR - heights[right];
+                }
+                right--;
+            }
+        }
+        return water;
+    }
+}
--- lintcode/669. Coin Change.java
+++ lintcode/669. Coin Change.java
@@ -7,26 +7,25 @@ public class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length; // number of coins
         // f[0], f[1], ..., f[amount]
-        int[] f = new int[amount + 1];
+        int[] dp = new int[amount + 1];
 
         // initialization
-        f[0] = 0;
+        dp[0] = 0;
 
         for (int i = 1; i <= amount; i++) {
-            f[i] = Integer.MAX_VALUE;
+            dp[i] = Integer.MAX_VALUE;
             // last coin A[k]
             for (int k = 0; k < n; k++) {
                 // boundary
-                if (i >= coins[k] && f[i - coins[k]] != Integer.MAX_VALUE) {
-                    f[i] = Math.min(f[i], f[i - coins[k]] + 1);
+                if (i >= coins[k] && dp[i - coins[k]] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[k]] + 1);
                 }
             }
         }
 
-        if (f[amount] == Integer.MAX_VALUE) {
+        if (dp[amount] == Integer.MAX_VALUE) {
             return -1;
-        } else {
-            return f[amount];
-        }
+        } 
+        return dp[amount];
     }
 }
