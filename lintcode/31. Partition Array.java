624cf5cf7b4637b6deda8e854ee0cd4ea3c58920
Waynexia888 <qiwuwaynexia@gmail.com>
2021-04-22 20:24:16 -0700

    Lintcode 31

--- lintcode/31. Partition Array.java
+++ lintcode/31. Partition Array.java
@@ -54,3 +54,46 @@ public class Solution {
 // [1, 2, 3]
 //     L     
 //  R
+
+
+/////////////////////////////////////////////////////
+public class Solution {
+    /**
+     * @param nums: The integer array you should partition
+     * @param k: An integer
+     * @return: The index after partition
+     */
+    public int partitionArray(int[] nums, int k) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            while (left <= right && nums[left] < k) {
+                left++;
+            } 
+
+            while (left <= right && nums[right] >= k) {
+                right--;
+            }
+
+            if (left <= right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        // if (nums[left] < k) {
+        //     return left + 1;
+        // }
+        return left;
+    }
+}
+
+// 1 2 2 3
+//   L
+// R 
