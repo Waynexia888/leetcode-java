84e72b98436f996e96265b917b31e19b7883f05d
Waynexia888 <qiwuwaynexia@gmail.com>
2021-04-22 18:57:37 -0700

    Lintcode 587

--- lintcode/587. Two Sum - Unique pairs.java
+++ lintcode/587. Two Sum - Unique pairs.java
@@ -35,3 +35,44 @@ public class Solution {
         return count;
     }
 }
+
+
+//////////////////////////////////////////////////////////
+public class Solution {
+    /**
+     * @param nums: an array of integer
+     * @param target: An integer
+     * @return: An integer
+     */
+    public int twoSum6(int[] nums, int target) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+        int count = 0;
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum < target) {
+                left++;
+            } else if (sum > target) {
+                right--;
+            } else {
+                count++;
+                left++;
+                right--;
+                //后面一个数跟前面一个数进行比较，如果相等，就跳过该位置
+                while (left < right && nums[left] == nums[left - 1]) {
+                    left++;
+                } 
+                while (left < right && nums[right] == nums[right + 1]) {
+                    right--;
+                }
+            }
+        }
+        return count;
+
+    }
+}
