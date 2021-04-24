3b7d44552044cac56ddf31f73991d0b3f3f7744e
Waynexia888 <qiwuwaynexia@gmail.com>
2021-04-23 17:22:41 -0700

    lintcode 148

--- lintcode/148. Sort Colors.java
+++ lintcode/148. Sort Colors.java
@@ -73,3 +73,40 @@ public class Solution {
         }
     }
 }
+
+////////////////////////////////////////////////////////////////
+
+public class Solution {
+    /**
+     * @param nums: A list of integer which is 0, 1 or 2 
+     * @return: nothing
+     */
+    public void sortColors(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+
+        int left = 0, right = nums.length - 1;
+        int index = 0;
+        while (index <= right) {
+            if (nums[index] == 0) {
+                swap(nums, index, left);
+                left++;
+                index++;
+            } else if (nums[index] == 1) {
+                index++;
+            } else {
+                swap(nums, index, right);
+                right--;
+            }
+
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
