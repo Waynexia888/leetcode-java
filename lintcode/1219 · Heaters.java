e82d3590c1e6dff5ec96337796a161e9f8b2be4c
Waynexia888 <qiwuwaynexia@gmail.com>
2021-04-29 17:11:02 -0700

    Lintcode 1219

---
+++ lintcode/1219 · Heaters.java
@@ -0,0 +1,37 @@
+public class Solution {
+    /**
+     * @param houses: positions of houses
+     * @param heaters: positions of heaters
+     * @return: the minimum radius standard of heaters
+     */
+    public int findRadius(int[] houses, int[] heaters) {
+        // Write your code here
+        int res = Integer.MIN_VALUE;
+        Arrays.sort(heaters);
+        for (Integer house : houses) {
+            int distanceToHeater = closestHeater(heaters, house);
+            res = Math.max(res, distanceToHeater);
+        }
+        return res;
+    }
+
+    private int closestHeater(int[] heaters, int house) {
+        int start = 0, end = heaters.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (heaters[mid] == house) {
+                return 0;
+            } else if (heaters[mid] < house) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        return Math.min(Math.abs(heaters[start] - house), Math.abs(heaters[end] - house));
+    }
+}
+
+// for each house, find the distance to the nearest heater by binary search,
+//     also update the minimum radius at the same time
+// houses -> n; heaters -> m 
+// m * log(m) + n * log(m) = O((m + n) * log(m))
