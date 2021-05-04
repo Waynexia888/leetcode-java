153d0ef2f6db6fabadc98f59f3e1842598e7f306
Waynexia888 <qiwuwaynexia@gmail.com>
2021-05-03 17:06:21 -0700

    Lintcode 900

---
+++ lintcode/900 · Closest Binary Search Tree Value.java
@@ -0,0 +1,35 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: the given BST
+     * @param target: the given target
+     * @return: the value in the BST that is closest to the target
+     */
+    public int closestValue(TreeNode root, double target) {
+        // time: O(n) 由于树的形状未知，所以最差的时间复杂度是O(n); space: O(1)
+        int closest = root.val;
+        while (root != null) {
+            if (Math.abs(root.val - target) < Math.abs(closest - target)) {
+                closest = root.val;
+            }
+
+            if (target < root.val) {
+                root = root.left;
+            } else {
+                root = root.right;
+            }   
+        }
+        return closest;
+    }
+}
