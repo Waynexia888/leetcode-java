dfeb0cfaf51f7daee5791dd7eb3798975a6ff3be
Waynexia888 <qiwuwaynexia@gmail.com>
2021-05-04 21:16:05 -0700

    Lintcode 68

--- lintcode/68. Binary Tree Postorder Traversal.java
+++ lintcode/68. Binary Tree Postorder Traversal.java
@@ -81,3 +81,44 @@ public class Solution {
         return results;
     }
 }
+
+////////////////////////////////////////////////////////////////////
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: A Tree
+     * @return: Postorder in ArrayList which contains node values.
+     */
+    public List<Integer> postorderTraversal(TreeNode root) {
+        // write your code here
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        if (root != null) {
+            stack.push(root);
+        }
+
+        while (!stack.isEmpty()) {
+             TreeNode node = stack.pop();
+            result.add(0, node.val);
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+        return result;
+    }
+}
