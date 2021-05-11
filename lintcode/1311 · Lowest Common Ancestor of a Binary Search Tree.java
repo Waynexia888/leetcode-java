0aa464ef1270fd2253c09a4023eba34d60e1ff81
Waynexia888 <qiwuwaynexia@gmail.com>
2021-05-10 19:59:53 -0700

    Lintcode 1311

---
+++ lintcode/1311 · Lowest Common Ancestor of a Binary Search Tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: root of the tree
+     * @param p: the node p
+     * @param q: the node q
+     * @return: find the LCA of p and q
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // write your code here
+        if (root == null) {
+            return null;
+        }
+
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        return root;
+    }
+}
