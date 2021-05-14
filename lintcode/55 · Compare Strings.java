f852dbff6326819c85e2c0cfdc376ad7b9bad209
Waynexia888 <qiwuwaynexia@gmail.com>
2021-05-14 03:46:59 -0700

    Lintcode 55

--- lintcode/55 · Compare Strings.java
+++ lintcode/55 · Compare Strings.java
@@ -30,3 +30,39 @@ public class Solution {
         return true;
     }
 }
+
+////////////////////////////////////////////////////////////////
+public class Solution {
+    /**
+     * @param A: A string
+     * @param B: A string
+     * @return: if string A contains all of the characters in B return true else return false
+     */
+    public boolean compareStrings(String A, String B) {
+        // write your code here
+        if (B.length() > A.length()) {
+            return false;
+        }
+        
+        int[] arrA = new int[26];
+        int[] arrB = new int[26];
+
+        char[] AA = A.toCharArray();
+        char[] BB = B.toCharArray();
+
+        for (char c : AA) {
+            arrA[c - 'A']++;
+        }
+
+        for (char c : BB) {
+            arrB[c - 'A']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (arrB[i] > arrA[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
