af89eff9f46ed64551593935c9c0d40435180b96
Waynexia888 <qiwuwaynexia@gmail.com>
2021-05-14 18:28:48 -0700

    Lintcode 80

--- lintcode/80. Median.java
+++ lintcode/80. Median.java
@@ -50,3 +50,58 @@ public class Median {
 
     }
 }
+
+////////////////////////////////////////////////////
+
+public class Solution {
+    /**
+     * @param nums: A list of integers
+     * @return: An integer denotes the middle number of the array
+     */
+    public int median(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int n = nums.length;
+        return quickSelect(nums, 0, n - 1, ((n + 1) / 2) - 1);
+    }
+
+    private int quickSelect(int[] nums, int start, int end, int k) {
+        if (start >= end) {
+            return nums[start];
+        }
+
+        int left = start, right = end;
+        int pivot = nums[(start + end) / 2];
+        while (left <= right) {
+            while (left <= right && nums[left] < pivot) {
+                left++;
+            }
+
+            while (left <= right && nums[right] > pivot) {
+                right--;
+            }
+
+            if (left <= right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        // xxxxxx0yyyyyy
+        // S    R L    E
+
+        if (k >= start && k <= right) {
+            return quickSelect(nums, start, right, k);
+        }
+
+        if (k >= left && k <= end) {
+            return quickSelect(nums, left, end, k);
+        }
+        return nums[right + 1];
+    }
+}
