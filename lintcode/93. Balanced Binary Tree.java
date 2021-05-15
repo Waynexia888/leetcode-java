8b3d9fc5a99529aba3e66c9ae09a14ea88b56807
Waynexia888 <qiwuwaynexia@gmail.com>
2021-05-14 20:10:01 -0700

    Lintcode 93

--- lintcode/93. Balanced Binary Tree.java
+++ lintcode/93. Balanced Binary Tree.java
@@ -136,3 +136,50 @@ public class Solution {
         
     }
 }
+
+///////////////////////////////////////////
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if this Binary tree is Balanced, or false.
+     */
+    public boolean isBalanced(TreeNode root) {
+        // write your code here
+        if (root == null) {
+            return true;
+        }
+
+        if (!isBalanced(root.left)) {
+            return false;
+        }
+
+        if (!isBalanced(root.right)) {
+            return false;
+        }
+        
+        if (Math.abs(height(root.left) - height(root.right)) > 1) {
+            return false;
+        }
+        return true;
+    }
+
+    private int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(height(root.left), height(root.right)) + 1;
+    }
+}
