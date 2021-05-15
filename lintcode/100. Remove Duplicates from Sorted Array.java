b6f14d059c261f7d4fc8798b5cff5072b16ac3d8
Waynexia888 <qiwuwaynexia@gmail.com>
2021-05-14 21:29:40 -0700

    Lintcode 100

--- lintcode/100. Remove Duplicates from Sorted Array.java
+++ lintcode/100. Remove Duplicates from Sorted Array.java
@@ -23,3 +23,65 @@ public class Remove_Duplicates_from_Sorted_Array {
         }
     }
 }
+
+///////////////////////////////////////////
+
+public class Solution {
+    /*
+     * @param nums: An ineger array
+     * @return: An integer
+     */
+    public int removeDuplicates(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int j = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != nums[i + 1]) {
+                j++;
+                nums[j] = nums[i + 1];
+            }
+        }
+        return j + 1;
+    }
+}
+
+// 1 1 1 2
+//     i
+//   j
+
+
+////////////////////////////////////////////////
+
+public class Solution {
+    /*
+     * @param nums: An ineger array
+     * @return: An integer
+     */
+    public int removeDuplicates(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int j = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        return j + 1;
+    }
+}
+
+// 1 1 1 2
+//       i
+//   j
+
+// i 指针负责遍历，寻找下一个不重复的位置
+// j 指针停在重复的位置
+
+
