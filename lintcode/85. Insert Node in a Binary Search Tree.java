b309049b769c48bb7b6018788648c2b23ff044e1
Waynexia888 <qiwuwaynexia@gmail.com>
2021-05-14 19:53:43 -0700

    Lintcode 85

--- lintcode/85. Insert Node in a Binary Search Tree.java
+++ lintcode/85. Insert Node in a Binary Search Tree.java
@@ -29,3 +29,52 @@ public class Solution {
         return root;
     }
 }
+
+///////////////////////////////////////////////////
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+
+public class Solution {
+    /*
+     * @param root: The root of the binary search tree.
+     * @param node: insert this node into the binary search tree
+     * @return: The root of the new binary search tree.
+     */
+    public TreeNode insertNode(TreeNode root, TreeNode node) {
+        // write your code here
+        if (root == null) {
+            return node;
+        }
+
+        TreeNode cur = root;
+        while (cur != null) {
+            if (cur.val > node.val) {
+                if (cur.left != null) {
+                    cur = cur.left;
+                } else {
+                    cur.left = node;
+                    break;
+                }
+            } else {
+                if (cur.right != null) {
+                    cur = cur.right;
+                } else {
+                    cur.right = node;
+                    break;
+                }
+            }
+        }
+        return root;
+    }
+}
