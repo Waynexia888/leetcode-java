7713be65fceaecd837f06b93a4ca452c373cc90b
Waynexia888 <qiwuwaynexia@gmail.com>
2021-06-03 22:41:25 -0700

    Lintcode 406

--- lintcode/406. Minimum Size Subarray Sum.java
+++ lintcode/406. Minimum Size Subarray Sum.java
@@ -1,3 +1,66 @@
+public class Solution {
+    /**
+     * @param nums: an array of integers
+     * @param s: An integer
+     * @return: an integer representing the minimum size of subarray
+     */
+    public int minimumSize(int[] nums, int s) {
+        // brute force, time: O(n^3); space: O(1), Time Limit Exceeded
+        int minLength = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                int sum = 0;
+                for (int k = i; k <= j; j++) {
+                    sum += nums[k];
+                }
+
+                if (sum >= s) {
+                    minLength = Math.min(minLength, j - i + 1);
+                }
+            }
+        }
+
+        if (minLength == Integer.MAX_VALUE) {
+            return -1;
+        }
+        return minLength;
+    }
+}
+
+/////////////////////////////////////////////////////////////////
+public class Solution {
+    /**
+     * @param nums: an array of integers
+     * @param s: An integer
+     * @return: an integer representing the minimum size of subarray
+     */
+    public int minimumSize(int[] nums, int s) {
+        // using prefixSum to simplify brute force, time: O(n^2); space: O(n)
+        int[] prefixSum = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+
+        int minLength = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                int sum = prefixSum[j + 1] - prefixSum[i];
+                if (sum >= s) {
+                    minLength = Math.min(minLength, j - i + 1);
+                }
+            }
+        }
+
+        if (minLength == Integer.MAX_VALUE) {
+            return -1;
+        }
+        return minLength;
+    }
+}
+
+//////////////////////////////////////////////////////////////////////
 public class Minimum_Size_Subarray_Sum {
     /**
      * @param nums: an array of integers
