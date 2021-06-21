9e038b16ecc9d6c5772a73c54648d16617733f93
Waynexia888 <qiwuwaynexia@gmail.com>
2021-06-20 17:11:10 -0700

    1815 · Threhold Alerts

--- wepay/1815 · Threhold Alerts.java
+++ wepay/1815 · Threhold Alerts.java
@@ -30,4 +30,32 @@ public class Solution {
 // 对于任意一段子数组(i, j)的和 -> prefixSum[j + 1] - prefixSum[i]
 // [2, 2, 2, 2, 5, 5, 5, 8]
 // [      2, 2, 3, 4, 5, 6]
-         
+
+///////////////////////////////////////////////////////////////////////
+
+public class Solution {
+    /**
+     * @param n: 
+     * @param k: 
+     * @param len: 
+     * @param num: //same as problem
+     * @return: //return long
+     */
+    public long solve(int n, int k, int len, int[] num) {
+        // time: O(n); space: O(1)
+        long count = 0;
+        long sum = 0;
+        for (int i = 0; i < num.length; i++) {
+            if (i < len) {
+                sum += num[i];
+            } else {
+                sum += num[i] - num[i - len];
+            }
+
+            if (i >= len - 1 && sum / len > k) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
