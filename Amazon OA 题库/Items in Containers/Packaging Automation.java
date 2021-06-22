62d13947e7f3970b315f4d66cfc9d8249790129e
Waynexia888 <qiwuwaynexia@gmail.com>
2021-06-22 01:31:51 -0700

    Items in Containers

--- Amazon OA 题库/Items in Containers/Packaging Automation.java
+++ Amazon OA 题库/Items in Containers/Packaging Automation.java
@@ -88,3 +88,28 @@ class Solution {
                 res.add(0);
         }
         return res;
+
+/////////////////////////////////////////////////////////////////////////
+
+public static List<Integer> numberOfItems(String s, List<Integer> startIndices, List<Integer> endIndices) {
+    int[] mem = new int[s.length()];
+    int count = 0;
+    for (int i = 0; i < s.length(); ++i) {
+        if (s.charAt(i) == '|') {
+            mem[i] = count;
+        } else {
+            ++count;
+        }
+    }
+    List<Integer> ans = new ArrayList<>();
+    for (int i = 0; i < startIndices.size(); ++i) {
+        int start = startIndices.get(i) - 1;
+        int end = endIndices.get(i) - 1;
+        
+        while (start < end && s.charAt(start) != '|') ++start;
+        while (start < end && s.charAt(end) != '|') --end;
+
+        ans.add(mem[end] - mem[start]);
+    }
+    return ans;
+}
