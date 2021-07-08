daf2fcf429b513dc4f46f8b6e7ca0233dea05253
Waynexia888 <qiwuwaynexia@gmail.com>
2021-07-07 20:40:33 -0700

    150. Evaluate Reverse Polish Notation

---
+++ wepay/电面 Phone Screening/150. Evaluate Reverse Polish Notation.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        // time: O(n); space: O(n)
+        Stack<Integer> stack = new Stack<>();
+        for (String s : tokens) {
+            if (s.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (s.equals("-")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a - b);
+            } else if (s.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (s.equals("/")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a / b);
+            } else {
+                stack.push(Integer.parseInt(s));
+            }
+        }
+        return stack.pop();
+    }
+}
