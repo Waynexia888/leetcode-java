cec5b2bd237bd61ce86c804c9a37b55e610dbc8b
Waynexia888 <qiwuwaynexia@gmail.com>
2021-07-07 21:45:00 -0700

    226. Invert Binary Tree

---
+++ wepay/电面 Phone Screening/226. Invert Binary Tree.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        invert(root);
+        return root;
+    }
+    
+    private void invert(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+        
+        invert(node.left);
+        invert(node.right);
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
