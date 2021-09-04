e2736541bd1da1cbbc108cddda69bc761a524fe6
Waynexia888 <qiwuwaynexia@gmail.com>
2021-09-04 15:01:53 -0700

    98. Validate Binary Search Tree

--- Google/Binary Tree/98. Validate Binary Search Tree.java
+++ Google/Binary Tree/98. Validate Binary Search Tree.java
@@ -31,3 +31,45 @@ class Solution {
         return dfs(root.left, low, root.val) && dfs(root.right, root.val, high);
     }
 }
+// time: o(n); space: o(n)
+
+////////////////////////////////////////
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private long pre = Long.MIN_VALUE;
+    public boolean isValidBST(TreeNode root) {
+        return inOrder(root);
+    }
+    
+    private boolean inOrder(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        
+        if (!inOrder(root.left)) {
+            return false;
+        }
+        
+        if (root.val <= pre) {
+            return false;
+        }
+        pre = root.val;
+        return inOrder(root.right);
+    }
+}
+
+// left, root, right
