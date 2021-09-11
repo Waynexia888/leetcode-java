09a983614ae2cb4f75c920777a6da5beaa63889a
Waynexia888 <qiwuwaynexia@gmail.com>
2021-09-10 22:47:54 -0700

    68 · Binary Tree Postorder Traversal

---
+++ Google/Binary Tree/68 · Binary Tree Postorder Traversal.java
@@ -0,0 +1,42 @@
+Lintcode 68 · Binary Tree Postorder Traversal
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: A Tree
+     * @return: Postorder in ArrayList which contains node values.
+     */
+    public List<Integer> postorderTraversal(TreeNode root) {
+        // write your code here
+        List<Integer> results = new ArrayList<>();
+        if (root == null) {
+            return results;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            results.add(0, node.val);
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+        return results;
+    }
+}
