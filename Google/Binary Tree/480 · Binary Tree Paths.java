073b36ab293e9025b3632468d94e8aa42c593add
Waynexia888 <qiwuwaynexia@gmail.com>
2021-09-10 19:37:59 -0700

    480 · Binary Tree Paths

---
+++ Google/Binary Tree/480 · Binary Tree Paths.java
@@ -0,0 +1,42 @@
+Lintcode 480 · Binary Tree Paths
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: the root of the binary tree
+     * @return: all root-to-leaf paths
+     */
+    public List<String> binaryTreePaths(TreeNode root) {
+        // write your code here
+        List<String> paths = new ArrayList<>();
+        if (root == null) {
+            return paths;
+        }
+
+        if (root.left == null && root.right == null) {
+            paths.add("" + root.val);
+        }
+
+        List<String> leftPaths = binaryTreePaths(root.left);
+        List<String> rightPaths = binaryTreePaths(root.right);
+
+        for (String path : leftPaths) {
+            paths.add(root.val + "->" + path);
+        }
+        for (String path : rightPaths) {
+            paths.add(root.val + "->" + path);
+        }
+        return paths;
+    }
+}
