98ae0f3ee7589a7e9fe3af065989a850aec5a896
Waynexia888 <qiwuwaynexia@gmail.com>
2021-09-16 13:08:19 -0700

    102 · Linked List Cycle

---
+++ Google/Stack & Queue & LinkedList/102 · Linked List Cycle.java
@@ -0,0 +1,40 @@
+Lintcode 102 · Linked List Cycle
+
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @return: True if it has a cycle, or false
+     */
+    public boolean hasCycle(ListNode head) {
+        // write your code here
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+// use two pointers, one is slow, one is fast
+// every time slow is moving one step, fast is moving two steps
+// at some point, the slow meet the fast, we can say it has a cycle in the linked list
