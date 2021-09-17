1fd75bde6efdaf9c67b15f2822dc0ebf44de94d1
Waynexia888 <qiwuwaynexia@gmail.com>
2021-09-17 01:39:05 -0700

    101. Symmetric Tree

--- Google/Binary Tree/101. Symmetric Tree.java
+++ Google/Binary Tree/101. Symmetric Tree.java
@@ -39,3 +39,52 @@ class Solution {
         return isMirror(n1.left, n2.right) && isMirror(n1.right, n2.left);
     }
 }
+
+/////////////////////////////////////////////////////////////////////
+iterative Way
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root.left);
+        queue.offer(root.right);
+        
+        while (!queue.isEmpty()) {
+            TreeNode n1 = queue.poll();
+            TreeNode n2 = queue.poll();
+            if (n1 == null && n2 == null) {
+                continue;
+            }
+            if (n1 == null || n2 == null) { // structure is not same
+                return false;
+            }
+            if (n1.val != n2.val) {
+                return false;
+            }
+            queue.offer(n1.left);
+            queue.offer(n2.right);
+            queue.offer(n1.right);
+            queue.offer(n2.left);
+        }
+        return true;
+    }
+}
