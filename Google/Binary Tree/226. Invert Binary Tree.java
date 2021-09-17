271ba204f1ea8b0cbca663a0dcdaa0bbf0c1966e
Waynexia888 <qiwuwaynexia@gmail.com>
2021-09-16 17:18:34 -0700

    226. Invert Binary Tree

--- Google/Binary Tree/226. Invert Binary Tree.java
+++ Google/Binary Tree/226. Invert Binary Tree.java
@@ -33,3 +33,77 @@ class Solution {
 }
 
 // since you need to invert the whole binary tree, we can divide the problem into smaller subproblems.
+
+///////////////////////////////////////////////////
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        return postOrder(root); 
+    }
+    
+    private TreeNode postOrder(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = postOrder(root.left);
+        TreeNode right = postOrder(root.right);
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        return root;
+    }
+}
+
+// since you need to invert the whole binary tree, we can divide the problem into smaller subproblems.
+
+//////////////////////////////////////////
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        postOrder(root); 
+        return root;
+    }
+    
+    private void postOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+    }
+}
+
+// since you need to invert the whole binary tree, we can divide the problem into smaller subproblems.
