48318d1db84872bcca65120eb87d8885ccfabd69
Waynexia888 <qiwuwaynexia@gmail.com>
2021-09-17 22:15:33 -0700

    300. Longest Increasing Subsequence

---
+++ Google/DP/300. Longest Increasing Subsequence.java
@@ -0,0 +1,34 @@
+Leetcode 300. Longest Increasing Subsequence
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        // state: dp[i]: the length of LIS ending in current index i
+        int n = nums.length;
+        int[] dp = new int[n];
+        
+        // initialization: dp[0..n - 1] = 1
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+        
+        // function: dp[i] = Math.max(dp[i], dp[j] + 1), where j < i && nums[j] < nums[i]
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        
+        // answer: Math.max(dp[0 .. n - 1])
+        int maxLength = 0;
+        for (int i = 0; i < n; i++) {
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+        return maxLength;
+    }
+}
