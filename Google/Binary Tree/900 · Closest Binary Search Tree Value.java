84510dd5b324634b519be8b889a3a43c0f7d69d7
Waynexia888 <qiwuwaynexia@gmail.com>
2021-09-19 15:34:49 -0700

    900 · Closest Binary Search Tree Value

--- Google/Binary Tree/900 · Closest Binary Search Tree Value.java
+++ Google/Binary Tree/900 · Closest Binary Search Tree Value.java
@@ -34,3 +34,45 @@ public class Solution {
         return closest;
     }
 }
+
+////////////////////////////////////////////////////////////////////
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: the given BST
+     * @param target: the given target
+     * @return: the value in the BST that is closest to the target
+     */
+    private int closest;
+    public int closestValue(TreeNode root, double target) {
+        // write your code
+        closest = root.val;
+        inorder(root, target);
+        return closest;
+    }
+
+    private void inorder(TreeNode root, double target) {
+        if (root == null) {
+            return;
+        }
+        if (target < root.val) {
+            inorder(root.left, target);
+        }
+        if (Math.abs(root.val - target) < Math.abs(closest - target)) {
+            closest = root.val;
+        }
+        inorder(root.right, target);
+    }
+}
