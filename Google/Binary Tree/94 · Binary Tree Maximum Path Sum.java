a092c9c493b320c5c285da490e6e4785aba7d88e
Waynexia888 <qiwuwaynexia@gmail.com>
2021-09-19 22:04:34 -0700

    124. Binary Tree Maximum Path Sum

--- Google/Binary Tree/94 · Binary Tree Maximum Path Sum.java
+++ Google/Binary Tree/94 · Binary Tree Maximum Path Sum.java
@@ -1,42 +1,39 @@
 Lintcode 94 · Binary Tree Maximum Path Sum
+Leetcode 124. Binary Tree Maximum Path Sum
 
 /**
- * Definition of TreeNode:
+ * Definition for a binary tree node.
  * public class TreeNode {
- *     public int val;
- *     public TreeNode left, right;
- *     public TreeNode(int val) {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
  *         this.val = val;
- *         this.left = this.right = null;
+ *         this.left = left;
+ *         this.right = right;
  *     }
  * }
  */
-
-public class Solution {
-    /**
-     * @param root: The root of binary tree.
-     * @return: An integer
-     */
-    private int maxPathSum = Integer.MIN_VALUE;
+class Solution {
+    private int maxSum;
     public int maxPathSum(TreeNode root) {
-        // write your code here
+        maxSum = Integer.MIN_VALUE;
         postOrder(root);
-        return maxPathSum;
+        return maxSum;
     }
-
+    
+    // 本层递归返回一条最长的路径和
     private int postOrder(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        int left = Math.max(0, postOrder(root.left));
-        int right = Math.max(0, postOrder(root.right));
-
-        int curMaxSum = Math.max(left, right) + root.val;
-        maxPathSum = Math.max(curMaxSum, Math.max(maxPathSum, left + right + root.val));
-        return curMaxSum;
+        
+        int left = Math.max(postOrder(root.left), 0);
+        int right = Math.max(postOrder(root.right), 0);
+        int curSum = left + right + root.val;
+        maxSum = Math.max(maxSum, curSum);
+        return Math.max(left, right) + root.val;
     }
 }
-
-// helper function returns root value plus the larger value of left subtree and right subtree.
-
-// update the "max" variable with the sum of root value and the largest value among left, right, left + right
